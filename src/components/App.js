b812e832d20d078ac62822dbc1c9f91e57d925e0
mary <Mariashee001@gmail.com>
2024-04-28 23:29:24 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
+
 import React from "react";
 import blogData from "../data/blog";
+import Article from "./Article";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
@@ -8,8 +13,14 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      <Header />
+      <About />
+      <ArticleList>
+        <Article />
+      </ArticleList>
     </div>
   );
 }
+
 
 export default App;
